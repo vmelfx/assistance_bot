df719cfde06f9d5ae6b34326c2c29fdd88ac9a54
vmelfx <vmelfx@protonmail.com>
2023-06-05 08:14:34 +0300

    start message handler

--- src/bot.py
+++ src/bot.py
@@ -5,9 +5,12 @@ import telebot
 bot = telebot.TeleBot(getenv("TELEBOT_API_KEY", default=""))
 
 
-@bot.message_handler(commands=["/start"])
+@bot.message_handler(commands=["start"])
 def start_handler(message):
-    bot.send_message(message.chat_id, f"Hello, {message.from_user.first_name}!")
+    bot.send_message(
+        message.chat.id,
+        f"Hello, {message.from_user.first_name} {message.from_user.last_name}!",
+    )
 
 
 bot.infinity_polling()
