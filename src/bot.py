b7590d395bad6beecd12bcd9435593d44acb90e6
Volodymyr Meleshchenko <vmelfx@protonmail.com>
2023-08-01 23:06:27 +0300

    Added "mark as done" handler

--- src/bot.py
+++ src/bot.py
@@ -32,12 +32,27 @@ def get_todo_list_on_today(session, message):
 
     formatted_message = []
     for todo in todoes:
-        formatted_message.append(f"<b>{todo.task_description}</b>\n")
+        if todo.complete:
+            formatted_message.append(f"<b><s>{todo.task_id}. {todo.task_description}</s></b>\n")
+        else:
+            formatted_message.append(f"<b>{todo.task_id}. {todo.task_description}</b>\n")
 
     bot.send_message(
         message.chat.id,
         "".join(formatted_message),
         parse_mode="HTML",
+    )
+
+
+@bot.message_handler(regexp=r"\d+ done")
+@transaction
+def mark_as_done(session, message):
+    received_task_id = message.text.split(" ")[0]
+    task = session.query(Todoes).filter(Todoes.task_id == received_task_id).first()
+    task.complete = True
+    bot.send_message(
+        message.chat.id,
+        f"{task.task_id} marked as done",
     )
 
 
