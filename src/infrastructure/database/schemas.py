6fc46c482f9102de32a7cc9c98bae355c848dbfa
Volodymyr Meleshchenko <vmelfx@protonmail.com>
2023-09-09 19:59:39 +0300

    Adjusted mypy to ignore SQLAlchemy

--- src/infrastructure/database/schemas.py
+++ src/infrastructure/database/schemas.py
@@ -1,24 +1,24 @@
 from sqlalchemy import Boolean, Column, Date, ForeignKey, Integer, String
-from sqlalchemy.orm import declarative_base, relationship  # type: ignore
+from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
 
 
-class User(Base):  # type: ignore
+class User(Base):
     __tablename__ = "user"
     chat_id = Column(Integer, primary_key=True)
-    tasks = relationship("Todoes", back_populates="user")  # type: ignore[var-annotated]
+    tasks = relationship("Todoes", back_populates="user")
 
     def __repr__(self):
         return f"User(chat_id={self.chat_id})"
 
 
-class Todoes(Base):  # type: ignore
+class Todoes(Base):
     __tablename__ = "todoes"
 
     task_id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey("user.chat_id"))
-    user = relationship("User", back_populates="tasks")  # type: ignore[var-annotated]
+    user = relationship("User", back_populates="tasks")
     date = Column("creation_date", Date)
     task_description = Column(String)
     priority = Column(Integer)
