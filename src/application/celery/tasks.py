f8d053e008e9f533fe49b8ace3e08a683b2f8382
Volodymyr Meleshchenko <vmelfx@protonmail.com>
2023-09-11 00:34:40 +0300

    Peridic task to notify of absence of undone tasks

--- src/application/celery/tasks.py
+++ src/application/celery/tasks.py
@@ -7,12 +7,23 @@ from src.infrastructure.telegram import bot
 
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
+    """Periodic task options such as time, name etc should be configured here"""
+
     sender.add_periodic_task(30.0, send_undone_todoes.s(), name='add every 10')
+    sender.add_periodic_task(
+        30.0,
+        send_daily_notification_to_users.s(),
+        name="send_daily_notification_to_user",
+    )
 
 
 @app.task()
 @transaction
 def send_undone_todoes(session):
+    """
+    This task is used to send to users notifications about undone tasks
+    """
+
     # Get users with undone tasks
     users_with_undone_tasks = (
         session.query(User)
@@ -24,17 +35,46 @@ def send_undone_todoes(session):
 
     for user in users_with_undone_tasks:
         # Get undone tasks for the user
-        tasks = [
+        undone_tasks = [
             TaskSubset(
                 task_id=task.task_id,
                 description=task.task_description,
             )
             for task in user.tasks
+            if not task.complete
         ]
 
         # Send notification to the user
         notification_message = "You have undone tasks. Here's your task list:\n"
-        for task in tasks:
+        for task in undone_tasks:
             notification_message += f"{task.task_id}. {task.description}\n"
 
+        bot.send_message(user.chat_id, notification_message)
+
+
+@app.task()
+@transaction
+def send_daily_notification_to_users(session):
+    """
+    This task is used to send to users notification about the absence of undone tasks
+    """
+
+    # Subquery to get get user's chat_id with undone tasks and then exlude them from the main query
+    users_without_undone_tasks_subquery = (
+        session.query(User.chat_id)
+        .join(User.tasks)
+        .filter(Todoes.complete.is_(False))
+        .distinct()
+        .subquery()
+    )
+
+    users_without_undone_tasks = (
+        session.query(User)
+        .filter(User.chat_id.notin_(users_without_undone_tasks_subquery))
+        .all()
+    )
+
+    notification_message = "You don't have any undone tasks! Congrats!"
+
+    for user in users_without_undone_tasks:
         bot.send_message(user.chat_id, notification_message)
---
+++ src/infrastructure/__init__.py
@@ -0,0 +1,4 @@
+"""
+This module includes all internal/external tools that are
+used for creating/adjusting the application.
+"""
